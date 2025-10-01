5561c9e8e001c6fe6a4aaf3bc8a2a87e3cae1171
Dguminski12 <davidguminski12@gmail.comgit config --global user.namegit config --global user.email davidguminski12@gmail.com>
2025-10-01 21:04:48 +0100

    title change on Portfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -6,7 +6,7 @@ import ProjectCard from "../components/ProjectCard.jsx";
 export default function Work() {
   return (
     <section className="work-page">
-      <h1>My Work</h1>
+      <h1>My Portfolio</h1>
       <p className="work-intro">
         A selection of projects across logo design, photo editing, and digital media. Click "View" below to see details and more images.
       </p>
